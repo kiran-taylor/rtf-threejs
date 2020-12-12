a5d0c353b90a7c0543c88a23baa6db2e0c797101
kiran.taylor04@gmail.com <kiran.taylor04@gmail.com>
2020-12-13 05:26:13 +0530

    character with same skin and unique animation

--- src/App.js
+++ src/App.js
@@ -1,73 +1,22 @@
-import * as THREE from "three";
-// import ReactDOM from "react-dom";
-import React, { Suspense, useEffect, useRef } from "react";
-import { Canvas, useFrame, useLoader } from "react-three-fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import "./App.css";
-import duck from "./Taunt.glb";
-
-import { OrbitControls } from "drei";
-
-const Model = (props) => {
-  const model = useLoader(GLTFLoader, duck);
-
-  // Here's the animation part
-  // *************************
-  let mixer;
-
-  if (model.animations.length) {
-    mixer = new THREE.AnimationMixer(model.scene);
-    console.log("mixer", mixer);
-    // console.log("arrayofmodels", arrModel);
-    model.animations.forEach((clip) => {
-      const action = mixer.clipAction(clip);
-      action.play();
-    });
-  }
-
-  useFrame((state, delta) => {
-    mixer?.update(delta);
-  });
-  // *************************
-
-  model.scene.traverse((child) => {
-    if (child.isMesh) {
-      child.castShadow = true;
-      child.receiveShadow = true;
-      child.material.side = THREE.FrontSide;
-    }
-  });
-
-  return <primitive object={model.scene} scale={props.scale} />;
-};
-
-const SpinBox = () => {
-  const mesh = useRef(null);
-
-  return (
-    <mesh castShadow ref={mesh}>
-      <planeBufferGeometry />
-    </mesh>
-  );
-};
-
+import Taunt from "./components/Taunt";
+import Walk from "./components/Walk";
+import Scoop from "./components/Scoop";
+import Flair from "./components/Flair";
 const App = () => {
   return (
-    <Canvas
-      camera={{ position: [500, 0, 0], fov: 60 }}
-      onCreated={({ gl, scene }) => {
-        // camera.lookAt(new THREE.Vector3(500, 0, 0));
-        scene.background = new THREE.Color("lightblue");
-
-        gl.shadowMap.enabled = true;
-        gl.shadowMap.type = THREE.PCFSoftShadowMap;
-      }}
-    >
-      <ambientLight />
-      <pointLight position={[0, 0, 200]} />
-      <Suspense fallback={<SpinBox />}>{<Model />}</Suspense>
-      <OrbitControls />
-    </Canvas>
+    <>
+      <div className="mainapp">
+        <div className="app">
+          <Taunt />
+          <Walk />
+        </div>
+        <div className="app">
+          <Scoop />
+          <Flair />
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Taunt.js
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame, useLoader } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import "./App.css";
+import duck from "./Taunt.glb";
+
+import { OrbitControls } from "drei";
+
+const Model = (props) => {
+  const model = useLoader(GLTFLoader, duck);
+
+  // Here's the animation part
+  // *************************
+  let mixer;
+
+  if (model.animations.length) {
+    mixer = new THREE.AnimationMixer(model.scene);
+    console.log("mixer", mixer);
+    // console.log("arrayofmodels", arrModel);
+    model.animations.forEach((clip) => {
+      const action = mixer.clipAction(clip);
+      action.play();
+    });
+  }
+
+  useFrame((state, delta) => {
+    mixer?.update(delta);
+  });
+  // *************************
+
+  model.scene.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+      child.material.side = THREE.FrontSide;
+    }
+  });
+
+  return <primitive object={model.scene} scale={props.scale} />;
+};
+
+const SpinBox = () => {
+  const mesh = useRef(null);
+
+  return (
+    <mesh castShadow ref={mesh}>
+      <planeBufferGeometry />
+    </mesh>
+  );
+};
+
+const Taunt = () => {
+  return (
+    <Canvas
+      camera={{ position: [500, 0, 0], fov: 60 }}
+      onCreated={({ gl, scene }) => {
+        // camera.lookAt(new THREE.Vector3(500, 0, 0));
+        scene.background = new THREE.Color("lightblue");
+
+        gl.shadowMap.enabled = true;
+        gl.shadowMap.type = THREE.PCFSoftShadowMap;
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[0, 0, 200]} />
+      <Suspense fallback={<SpinBox />}>{<Model />}</Suspense>
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default Taunt;
