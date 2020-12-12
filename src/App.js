61c758388324ee20527d80a1d6d67c66d10624c1
kiran.taylor04@gmail.com <kiran.taylor04@gmail.com>
2020-12-13 03:54:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from "three";
+// import ReactDOM from "react-dom";
+import React, { Suspense, useEffect, useRef } from "react";
+import { Canvas, useFrame, useLoader } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import "./App.css";
+import duck from "./Taunt.glb";
 
-function App() {
+import { OrbitControls } from "drei";
+
+const Model = (props) => {
+  const model = useLoader(GLTFLoader, duck);
+
+  // Here's the animation part
+  // *************************
+  let mixer;
+
+  if (model.animations.length) {
+    mixer = new THREE.AnimationMixer(model.scene);
+    console.log("mixer", mixer);
+    // console.log("arrayofmodels", arrModel);
+    model.animations.forEach((clip) => {
+      const action = mixer.clipAction(clip);
+      action.play();
+    });
+  }
+
+  useFrame((state, delta) => {
+    mixer?.update(delta);
+  });
+  // *************************
+
+  model.scene.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+      child.material.side = THREE.FrontSide;
+    }
+  });
+
+  return <primitive object={model.scene} scale={props.scale} />;
+};
+
+const SpinBox = () => {
+  const mesh = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh castShadow ref={mesh}>
+      <planeBufferGeometry />
+    </mesh>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Canvas
+      camera={{ position: [500, 0, 0], fov: 60 }}
+      onCreated={({ gl, scene }) => {
+        // camera.lookAt(new THREE.Vector3(500, 0, 0));
+        scene.background = new THREE.Color("lightblue");
+
+        gl.shadowMap.enabled = true;
+        gl.shadowMap.type = THREE.PCFSoftShadowMap;
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[0, 0, 200]} />
+      <Suspense fallback={<SpinBox />}>{<Model />}</Suspense>
+      <OrbitControls />
+    </Canvas>
+  );
+};
 
 export default App;
